f1583290d9cbc5279743ca546124212f34e4a0e6
Nandhu Krishna <nandhukrishna393@gmail.com>
2024-09-13 22:08:58 +0530

    solving problems

--- #01-Arrays/allIInOne.js
+++ #01-Arrays/allIInOne.js
@@ -26,3 +26,23 @@ function quickSort(arr, prop) {
   console.log(quickSort(objectsArray, 'age'));
   // Output: [{name: 'Jane', age: 22}, {name: 'John', age: 25}, {name: 'Emily', age: 28}, {name: 'Tom', age: 30}]
   
+
+
+  function countSubarraysWithSumK(arr, k) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+      let sum = 0;
+      for (let j = i; j < arr.length; j++) {
+        sum += arr[j];
+        if (sum === k) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+  
+  let arr = [1, 2, 3, 4, 5];
+  let k = 5;
+  console.log(countSubarraysWithSumK(arr, k)); // 2 ([2,3] and [5])
+  
