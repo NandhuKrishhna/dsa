42fdae72e34509e20176e03fef9edf43bd00a524
Nandhu Krishna <nandhukrishna393@gmail.com>
2024-09-13 22:12:19 +0530

    solving problems

--- #01-Arrays/allIInOne.js
+++ #01-Arrays/allIInOne.js
@@ -90,11 +90,11 @@
   
 //   let arr = [0, 1, 0, 1, 1, 0, 0];
 //   console.log(maxLenEqualZeroOne(arr)); 
-function uniquePaths(m, n) {
-    if (m === 1 || n === 1) return 1;
-    return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
-  }
+// function uniquePaths(m, n) {
+//     if (m === 1 || n === 1) return 1;
+//     return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+//   }
   
-  console.log(uniquePaths(3, 2)); // 3
-  console.log(uniquePaths(3, 3)); // 6
+//   console.log(uniquePaths(3, 2)); // 3
+//   console.log(uniquePaths(3, 3)); // 6
   
