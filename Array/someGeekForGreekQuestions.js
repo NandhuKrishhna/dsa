e3c426a750404057e1e5e835eb63856ac66d56ee
Nandhu Krishna <nandhukrishna393@gmail.com>
2024-09-17 18:51:20 +0530

    min and max in an array

--- Array/someGeekForGreekQuestions.js
+++ Array/someGeekForGreekQuestions.js
@@ -1,18 +1,16 @@
-// function* findThePeekNumber(arr){
-//     let n = arr.length;
+function findThePeekNumber(arr){
+    let n = arr.length;
 
-//     if(n>2){
-//         for (let i = 1; i < n-1; i++) {
-//          if(arr[i] >=arr[i-1] && arr[i]>=arr[i+1]){
-//             yield arr[i];
-//          }            
-//         }
-//     }else{
-//         console.log("There is no Peak Number");
-//     }
-// }
+    if(n>2){
+        for (let i = 1; i < n-1; i++) {
+         if(arr[i] >=arr[i-1] && arr[i]>=arr[i+1]){
+            console.log(arr[i]);
+         }            
+        }
+    }else{
+        console.log("There is no Peak Number");
+    }
+}
 
-// const result = findThePeekNumber([1,2,1,3,4,5,3])
-// for(const res of result){
-//     console.log(res);
-// }
+
+//Recursion>>>>>>>>>>>>>>>>>>>>>>
