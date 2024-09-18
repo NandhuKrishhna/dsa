0a9844844add6533d3b87a6a29f6927e4a258d6b
Nandhu Krishna <nandhukrishna393@gmail.com>
2024-09-18 10:35:53 +0530

    liner serach

---
+++ Array/linear-search.js
@@ -0,0 +1,27 @@
+const linerSearch = function(nums,val){
+    let n = nums.length;
+    for( let i = 0 ; i < n  ; i++){
+        console.log('nums[i] : ',nums[i],'val :',val);
+        if(nums[i] === val){
+            return i; 
+        }
+    }
+    return -1
+}
+
+console.log(linerSearch([1],1));
+
+const globalLinerSearch = function (nums,val){
+    let result = new Array;
+    for( let i = 0 ; i < nums.length ; i++){
+        if(nums[i] === val){
+            result.push(i)
+        }
+    }
+    if(result.length === 0 ) return -1
+    return result
+    
+
+
+}
+console.log(globalLinerSearch([1,1,2,3,4,5,67,8,8,],8));
