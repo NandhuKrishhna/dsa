ac5cbe839f4ffbc1fbf49c3142999bcacfd1b7b2
Nandhu Krishna <nandhukrishna393@gmail.com>
2024-09-21 12:44:19 +0530

    created a selection sort file

--- Bubble-sorting/bubble-sorting.js
+++ Bubble-sorting/bubble-sorting.js
@@ -1,24 +1,42 @@
-function bubbleSort(arr){
-    let  n = arr.length, swapped ;
+// function bubbleSort(arr){
+//     let  n = arr.length, swapped ;
     
-    for (let i = 0; i < n; i++) {
-        swapped = false;
-      for (let j = 0; j < n - 1 - i; j++) {
-        if(arr[j]> arr[j+1]){
-            let temp = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
-            swapped = true;
+//     for (let i = 0; i < n; i++) {
+//         swapped = false;
+//       for (let j = 0; j < n - 1 - i; j++) {
+//         if(arr[j]> arr[j+1]){
+//             let temp = arr[j];
+//             arr[j] = arr[j+1];
+//             arr[j+1] = temp;
+//             swapped = true;
+//         }
+//       } 
+//       if(!swapped){
+//         break;
+//       } 
+//     }
+//     return arr
+
+// }
+
+let arr = [0,1,1,2,2,0]
+// const result =  bubbleSort(array);
+// console.log(result);
+
+
+const bubblesort = (arr)=> {
+    let n = arr.length-1, swapped ;
+    for(let i = 0 ; i< n ; i++){
+      swapped = false;
+      for(let j = 0 ; j < n -1-i; j ++){
+        if(arr[j]>arr[j+1]){
+            [arr[j],arr[j+1]]=[arr[j+1],arr[j]];
+            swapped = true
         }
-      } 
-      if(!swapped){
-        break;
-      } 
+      }
+      if(!swapped) break;
     }
     return arr
-
 }
 
-let array = [64, 34, 25, 12, 22, 11, 90];
-const result =  bubbleSort(array);
-console.log(result);
+console.log(bubblesort(arr));
