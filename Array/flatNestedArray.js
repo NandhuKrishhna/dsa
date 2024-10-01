b73fa4e98d2722306a3286f0ed69d56d84b328c9
Nandhu Krishna <nandhukrishna393@gmail.com>
2024-10-01 21:44:27 +0530

    moving to Learn Heap

--- Array/flatNestedArray.js
+++ Array/flatNestedArray.js
@@ -1,20 +1,18 @@
 function flattenArrayRecursive(arr) {
-    let result = [];
-    function flattenHelper(subArray) {
-        for (let i = 0; i < subArray.length; i++) {
-            if (typeof subArray[i] === 'object' && subArray[i] !== null) {
-                flattenHelper(subArray[i]);
-            } else {
-                result.push(subArray[i])
-            }
-        }
+  let result = [];
+  function flattenHelper(subArray) {
+    for (let i = 0; i < subArray.length; i++) {
+      if (typeof subArray[i] === "object" && subArray[i] !== null) {
+        flattenHelper(subArray[i]);
+      } else {
+        result.push(subArray[i]);
+      }
     }
+  }
 
-    flattenHelper(arr);
-    return result;
+  flattenHelper(arr);
+  return result;
 }
 
-
 const nestedArray = [1, [2, [3, 4], 5], 6];
-console.log(flattenArrayRecursive(nestedArray)); 
-
+console.log(flattenArrayRecursive(nestedArray));
